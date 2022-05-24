8f12b6a9fd318ee7470bc703e6433f345a064985
DrWatt <calumwatt12@outlook.com>
2022-05-24 08:50:08 +0800

    Create agrobasepractice.R

---
+++ R/agrobasepractice.R
@@ -0,0 +1,12 @@
+#' Practice Agrobase file.
+#'
+#' A dataset representing a direct Agrobase download that we wish
+#' to convert to FieldScorer format.
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   ...
+#' }
+"agrobasepractice"
